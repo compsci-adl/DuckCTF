77c8e41606155afa280a9a0bea5b0a339902f011
0xRF <30642254+0xRF@users.noreply.github.com>
2022-05-09 02:15:08 +0930

    2 am pwn challenge yum

---
+++ pwn/retToQuack/ret_to_quack.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void do_stuff() {
+  char buff[32];
+
+  puts("Welcome");
+  printf("the duck gives you a shout %p\n", (void *)puts);
+  read(0, buff, 256);
+  puts("/bin/cat flag.txt");
+}
+
+void rop2() {}
+
+void rop() {
+  asm("pop %rdi");
+  asm("ret");
+}
+
+int main(int argc, char **argv) {
+  setbuf(stdout, NULL);
+  setbuf(stdin, NULL);
+  setbuf(stderr, NULL);
+  do_stuff();
+
+  return 0;
+}
